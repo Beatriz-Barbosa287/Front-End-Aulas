e8937f8ab74f42954ceb98072be6efee3cf273b0
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-02-28 11:27:40 -0300

    28/02/2025

---
+++ DOM/Exercicio1DOM/script.js
@@ -0,0 +1,8 @@
+let titulo = document.getElementsByTagName("h1");
+console.log(titulo);
+
+
+let parágrafo = document.getElementsByClassName("paragrafo");
+console.log(parágrafo);
+
+let botao = document
