5464dd671be7a2b1424db1e9cfcb5dc4415340ba
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-03-07 11:20:21 -0300

    07/03/2025

--- DOM/sa01todolist/script.js
+++ DOM/sa01todolist/script.js
@@ -1,24 +1,26 @@
-//LISTA DE TAREFAS TO DO LIST
+//Lista de Taferas (ToDoList)
 
-//dispara o evento 
-document.getElementById("btnAdiocionar").addEventListener("click", criarTarefa);
+// dispara o evento
+document.getElementById("btnAdicionar").addEventListener("click",criarTarefa);
 
-function criarTarefa() {
+function criarTarefa(){
     let input = document.getElementById("tarefa");
-    let texto = input.value.trim(); //remove os espaços antes e depois do texto
-//verificar se a tarefa está vazia 
-if (texto === ""){
-    return;
-}
-let li = document.createElement("li");
-li = texto + '<button onclick="removerTarefa (this)"> Remover </button>'
-//adicionar p li -> ul
-let ul =document.getElementById("Lista");
-ul.appendChild(li);
-input.value= " "; //limpa o campo do Input
+    let texto = input.value.trim();//remove os espaços antes e depois do texto
+    //verificar se a tafera não esta vazia
+    if(texto===""){
+        return;
+    }
+    //se não vazio
+    let li = document.createElement("li");
+    li.innerHTML = texto+'<button onclick="removerTarefa(this)">Remover</button>';
+    //adicionar o li -> ul
+    let ul = document.getElementById("lista");//pega a lista
+    ul.appendChild(li);//adiciona o li a ul
+
+    input.value = ""; //limpa  campo do input
 
 }
 
-function removerTarefa(botao) {
+function removerTarefa(botao){
     botao.parentElement.remove();
 }
