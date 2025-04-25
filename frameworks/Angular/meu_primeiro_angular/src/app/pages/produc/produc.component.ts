c2722eb311d85d7e76b2e12d259d2cd781f09277
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-04-25 11:14:16 -0300

    25/04/2025

---
+++ frameworks/Angular/meu_primeiro_angular/src/app/pages/produc/produc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produc',
+  imports: [],
+  templateUrl: './produc.component.html',
+  styleUrl: './produc.component.css'
+})
+export class ProducComponent {
+
+}
