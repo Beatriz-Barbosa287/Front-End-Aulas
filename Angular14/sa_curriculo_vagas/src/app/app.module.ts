42a233fa5c200cdf9a7f2525db279adba970aedf
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-05-16 09:47:47 -0300

    16/05/2025

---
+++ Angular14/sa_curriculo_vagas/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './view/inicio/inicio.component';
+import { CurriculoComponent } from './view/curriculos/curriculo.component';
+import { VagasComponent } from './view/vagas/vagas.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    CurriculoComponent,
+    VagasComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
