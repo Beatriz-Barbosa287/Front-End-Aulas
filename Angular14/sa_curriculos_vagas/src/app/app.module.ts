4a18037322cb2dddb34d066a9fe31e8e6316066c
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-05-16 09:12:11 -0300

    16/05/2025

--- Angular14/sa_curriculos_vagas/src/app/app.module.ts
+++ Angular14/sa_curriculos_vagas/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './templates/header/header.component';
-import { FooterComponent } from './templates/footer/footer.component';
-import { CurriculosComponent } from './view/curriculos/curriculos.component';
-import { InicioComponent } from './view/inicio/inicio.component';
-import { VagasComponent } from './view/vagas/vagas.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    CurriculosComponent,
-    InicioComponent,
-    VagasComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
